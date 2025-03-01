949567e723ff40154a98d738faa71b81686e11f9
henrickyL <henrickyl1@gmail.com>
2025-03-01 12:00:44 -0300

    adjust registry

--- src/lib/registry.tsx
+++ src/lib/registry.tsx
@@ -1,34 +1,21 @@
-// src/lib/registry.tsx
 "use client";
+import { useServerInsertedHTML } from "next/navigation";
+import { useState } from "react";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 
-import React from "react";
-import { ServerStyleSheet } from "styled-components";
-
-// Este componente agora gerencia a coleta dos estilos sem sobrescrever createElement
-export default function StyledComponentsRegistry({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const sheet = new ServerStyleSheet();
+export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
 
   if (typeof window === "undefined") {
-    // Colete os estilos enquanto renderiza os filhos
-    const collectedStyles = sheet.collectStyles(<>{children}</>);
-
-    return (
-      <>
-        {collectedStyles}
-        {/* Adiciona os estilos gerados no SSR */}
-        <style
-          dangerouslySetInnerHTML={{
-            __html: sheet.getStyleTags(),
-          }}
-        />
-      </>
-    );
+    useServerInsertedHTML(() => {
+      const styles = styledComponentsStyleSheet.getStyleElement();
+      styledComponentsStyleSheet.instance.clearTag();
+      return <>{styles}</>;
+    });
+  
+    return <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>;
   }
-
+  
   // No lado do cliente, basta renderizar normalmente
   return <>{children}</>;
 }
