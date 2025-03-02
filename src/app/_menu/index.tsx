f5da4c41a3de69cc6e98868482d5e6496e505d84
henrickyL <henrickyl1@gmail.com>
2025-03-02 16:05:45 -0300

    adjusts router

---
+++ src/app/_menu/index.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { Menu } from "@/components/Menu";
+import { NavBar } from "@/components/NavBar";
+import { IconType } from "react-icons";
+import { BsFillMortarboardFill as FormationIcon } from "react-icons/bs";
+import { BsBriefcaseFill as ExperienceIcon } from "react-icons/bs";
+import { BsFillHouseFill as HomeIcon } from "react-icons/bs";
+import { BsFillFolderFill as ProjectIcon } from "react-icons/bs";
+import { BsEnvelopeFill as ContactIcon } from "react-icons/bs";
+import styled from "styled-components";
+
+
+
+interface MenuItem{
+  title: string,
+  icon: IconType
+  href: string
+}
+
+const menuItems: MenuItem[] = [
+  {
+    title: "Home",
+    icon: HomeIcon,
+    href: "/"
+  },
+  {
+    title: "Projetos",
+    icon: ProjectIcon,
+    href: "/projects"
+  },
+  {
+    title: "Experiências",
+    icon: ExperienceIcon,
+    href: "/experiences"
+  },
+  {
+    title: "Formação",
+    icon: FormationIcon,
+    href: "/"
+  },
+  {
+    title: "Contato",
+    icon: ContactIcon,
+    href: "/contact"
+  },
+
+]
+
+
+const Nav = ()=>{
+  return(
+    <NavBar.Root>
+        {menuItems.map((item,i)=>
+          <NavBar.Link key={i} href={item.href} title={item.title} icon={item.icon} />
+        )}
+    </NavBar.Root>
+  )
+}
+
+const Nav2 = ()=>{
+  return(
+    <Menu.Root>
+        {menuItems.map((item,i)=>
+          <Menu.Item key={i} href={item.href} title={item.title} icon={item.icon} />
+        )}
+    </Menu.Root>
+  )
+}
+
+const TestNavsSty = styled.div`
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid ${prop=>prop.theme.contrast};
+    padding: 4px;
+`
+
+
+export const TestNavs = ()=>{
+    return(
+    <TestNavsSty>
+        <Nav />
+        <Nav2 />
+    </TestNavsSty>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,11 @@
 "use client";
-import { Container } from "@/components/Container";
-import { NavBar } from "@/components/NavBar";
 import React from "react";
-
-
-import { CiFolderOn as Folder} from "react-icons/ci";
-import { CiCircleInfo as Info } from "react-icons/ci";
-
-
-
-const Nav = ()=>{
-  return(
-    <NavBar.Root>
-        <NavBar.Link href="/" title="Home" icon={Folder} />
-        <NavBar.Link href="about" title="Sobre" icon={Info}/>
-    </NavBar.Root>
-  )
-}
-
 
 const Home = () => {
   return (
-    <Container.Root>
-      <Container.Center>
-        <Nav />
-      </Container.Center>
-    </Container.Root>
+    <>
+      <h1>Home</h1>
+    </>
   );
 };
 
