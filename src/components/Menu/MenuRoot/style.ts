5e8753be9138f674b73d970eab84add2719e3b47
henrickyL <henrickyl1@gmail.com>
2025-03-03 17:57:06 -0300

    swing indicator & adjust

--- src/components/Menu/MenuRoot/style.ts
+++ src/components/Menu/MenuRoot/style.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const MenuRootSty =styled.nav`
     position: relative;
@@ -11,18 +11,32 @@ export const MenuRootSty =styled.nav`
     padding: 8px 12px;
     gap: 8px;
     background-image: url(${(props) => props.theme.noise});
-    border: 1px solid ${prop=>prop.theme.contrastLight};
+    /* border: 1px solid ${prop=>prop.theme.contrastLight}; */
+    margin-top:4px;
 `
 
-export const Indicator = styled.div<{index:number, title:string}>`
+const swing = keyframes`
+    0% { transform: rotateZ(-4deg); }
+    50% { transform: rotateZ(4deg); }
+    100% { transform: rotateZ(-4deg); }
+`;
+
+
+export const Indicator = styled.div<{$index:number, title:string}>`
     position: absolute;
     display: flex;
     justify-content: center;
     align-items: center;
     width: 4rem;
     height: 4rem;
+    /*  */
     transition: 0.5s;
-    transform: translateX(calc(${prop=>prop.index}*3rem));
+    transform-origin: top center;
+    left: calc(${prop=>prop.$index}*3rem);
+
+    &.transition-end {
+        animation: ${swing} 5s ease-in-out infinite;
+    }
 
     &::before{
         content: "";
@@ -43,7 +57,18 @@ export const Indicator = styled.div<{index:number, title:string}>`
         transition: 0.4s;
         letter-spacing: 0.05em;
         text-transform: uppercase;
-        opacity: 0.;
+        opacity: 0;
+    }
+
+    &::after{
+        content: "";
+        position: absolute;
+        top: 0;
+        width: 0.5rem;
+        height: 0.5rem;
+        background-color: transparent;
+        box-shadow: 0 0 0 2px ${prop=>prop.theme.primary};
+        border-radius: 50%;
     }
 
     &.transition-end::before {
