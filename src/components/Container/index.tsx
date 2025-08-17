a6d78de9dc2fda3fef57faa6ffaca16a9e154cee
HenrickyL <Henrickyl1@gmail.com>
2025-08-17 12:19:04 -0300

    feat: add Container bottom  adjusts

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,3 +1,4 @@
+import { ContainerBottom } from "./ContainerBottom";
 import { ContainerCenter } from "./ContainerCenter";
 import { ContainerRoot } from "./ContainerRoot";
 import { ContainerTopCenter } from "./ContainerTopCenter";
@@ -5,5 +6,6 @@ import { ContainerTopCenter } from "./ContainerTopCenter";
 export const Container = {
     Root: ContainerRoot,
     Center: ContainerCenter,
-    TopCenter: ContainerTopCenter
+    TopCenter: ContainerTopCenter,
+    Bottom: ContainerBottom
 }
