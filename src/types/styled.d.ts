c0a7924dc4baf6c21de8930cdfe2008250454563
Henricky Monteiro <henrickyl1@gmail.com>
2026-05-19 12:06:40 -0300

    fix: remove

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -1,7 +1,7 @@
-/* eslint-disable @typescript-eslint/no-empty-object-type */
-import "styled-components";
-import { Theme } from "./theme";
+// /* eslint-disable @typescript-eslint/no-empty-object-type */
+// import "styled-components";
+// import { Theme } from "./theme";
 
-declare module "styled-components" {
-  export interface DefaultTheme extends Theme {}
-}
+// declare module "styled-components" {
+//   export interface DefaultTheme extends Theme {}
+// }
